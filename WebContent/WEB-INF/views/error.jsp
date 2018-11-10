02bba5fb7d119ea336d44c0f35f20aedd9ad3e37
Mario Moraño Orviz <moraorviz@gmail.com>
2018-11-10 18:03:00 +0100

    first commit

---
+++ WebContent/WEB-INF/views/error.jsp
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Amazin</title>
+</head>
+<body>
+	<h1 style="text-align: center;">AMAZIN.COM</h1>
+	<br>
+	<h2 style="text-align: center;">Welcome to the smallest virtual shop in
+		the world!</h2>
+	<br />
+	<br />
+	<b>Uppppssss... We have a problem!</b>
+
+</body>
+</html>
---
+++ src/main/java/impl/mario/presentacion/BienvenidaController.java
@@ -0,0 +1,31 @@
+package impl.mario.presentacion;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+public class BienvenidaController {
+
+	/*
+	 * Setting / as request mapping url we are setting the default controller for
+	 * the application.
+	 */
+	@RequestMapping("/")
+	public String bienvenida() {
+		System.out.println("Ejecutando controlador de bienvenida");
+		return "redirect:/menu";
+	}
+
+	@RequestMapping("/menu")
+	public String index() {
+		System.out.println("Executing private/menu controller for user ");
+		return "indice";
+	}
+
+}
+
